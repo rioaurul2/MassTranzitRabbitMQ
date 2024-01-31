79e031cf3415310dd7ba041abfba70c0b26f5ceb
rioaurul2 <pmarin08@yahoo.com>
2024-01-31 21:34:34 +0200

    add getting started and multi bus

---
+++ GettingStarted/Worker.cs
@@ -0,0 +1,33 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GettingStarted
+{
+    public class Worker : BackgroundService
+    {
+        readonly IBus _bus;
+
+        public Worker(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                await _bus.Publish(new HelloMessage
+                {
+                    Name = "M",
+                }, stoppingToken);
+
+                await Task.Delay(1000, stoppingToken);
+            }
+
+            
+        }
+    }
+}
---
+++ MultiBusApp/MultiBusApp/SimpleConsumer.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+
+public class SimpleConsumer : IConsumer<SimpleMessage>
+{
+    readonly ILogger<SimpleConsumer> _logger;
+
+    public SimpleConsumer( ILogger<SimpleConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<SimpleMessage> context)
+    {
+        _logger.LogInformation("Consumed");
+
+        return Task.CompletedTask;
+    }
+}
