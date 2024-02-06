24fd2ce17d20ac53e1af1040d0266f8230208f87
rioaurul2 <pmarin08@yahoo.com>
2024-02-06 20:14:50 +0200

    t5up

---
+++ MassTransitUsingRabbitMQInASPDotNET/Mediator/MassTransitMediator/MassTransitMediator/Program.cs
@@ -0,0 +1,68 @@
+﻿using MassTransit;
+using MassTransit.Mediator;
+
+namespace MassTransitMediator
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IMediator mediator = Bus.Factory.CreateMediator(config =>
+            {
+                config.Consumer<SubmitOrderConsumer>();
+                config.Consumer<OrderStatusConsumer>();
+
+                //var handle = mediator.ConnectConsumer<SubmitOrderConsumer>();
+            });
+
+            Guid orderId = NewId.NextGuid();
+
+            await mediator.Send<SubmitOrder>(new { OrderId = orderId });
+
+            var client = mediator.CreateRequestClient<GetOrderStatus>();
+
+            var response = await client.GetResponse<OrderStatus>(new { OrderId = orderId });
+
+            Console.WriteLine($"Order status {0}", response.Message.Status);
+        }
+    }
+
+    public interface GetOrderStatus
+    {
+        Guid OrderId { get; }
+    }
+
+    public interface SubmitOrder
+    {
+        Guid OrderId { get; }
+    }
+
+    public interface OrderStatus
+    {
+        Guid OrderId { get; }
+        string Status { get; }
+    }
+
+    class OrderStatusConsumer : IConsumer<GetOrderStatus>
+    {
+        public async Task Consume(ConsumeContext<GetOrderStatus> context)
+        {
+            await context.RespondAsync<OrderStatus>(new
+            {
+                context.Message.OrderId,
+                Status = "Pending"
+            });
+        }
+    }
+
+    class SubmitOrderConsumer : IConsumer<SubmitOrder>
+    {
+        public async Task Consume(ConsumeContext<SubmitOrder> context)
+        {
+            await context.RespondAsync<SubmitOrder>(new
+            {
+                context.Message.OrderId
+            });
+        }
+    }
+}
